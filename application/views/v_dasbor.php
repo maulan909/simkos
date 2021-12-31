5ffc8fe0683a4d5c1c9cab9bd84cb223903be2f3
maulan909 <maulanahilmi909@gmail.com>
2021-12-31 09:43:14 +0700

    initial commit

---
+++ application/views/v_dasbor.php
@@ -0,0 +1,38 @@
+            <!-- START PAGE CONTENT -->
+            <div class="fade-in-up">
+                <div class="row">
+                    <div class="col-lg-12" style="padding: 0 0 15px 0;">
+                            <!--Carousel Wrapper-->
+                            <div id="slide-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+                                <!--Slides-->
+                                <div class="carousel-inner" role="listbox">
+                                    <div class="carousel-caption">
+                                        <h3 class="h3-responsive big-brand1">SELAMAT DATANG</h3>
+                                        <p class="big-brand2">DI SISTEM INFORMASI INDEKOS IBU IDA</p>
+                                    </div>
+                                    <div class="carousel-item active">
+                                        <div class="view">
+                                            <img class="d-block" src="<?= base_url('assets/img/kos/1.jpg')  ?>"
+                                            alt="First slide" size="40x40">
+                                            <div class="mask rgba-black-light"></div>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <div class="view">
+                                            <img class="d-block" src="<?= base_url('assets/img/kos/2.jpg')  ?>"
+                                            alt="First slide" size="40x40">
+                                            <div class="mask rgba-black-light"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--/.Slides-->
+                                <a class="carousel-control-next" href="#slide-carousel" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                            <!--/.Carousel Wrapper-->
+                    </div>
+                </div>
+            </div>
+            <!-- END PAGE CONTENT-->
